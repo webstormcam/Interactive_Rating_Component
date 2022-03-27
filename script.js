c98fa8868ab2bf944ae9e6639c11f7aa6cbd537c
Cameron Chandler <webstormcam@gmail.com>
2022-03-27 09:49:41 -0400

    Made the first slide

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let selection;
+const allChoices = document.querySelectorAll('.circle');
+for (let i = 0; i < allChoices.length; i++) {
+    allChoices[i].addEventListener('click',function(event) {
+      var prevRadio = document.querySelector('.circle-clicked');
+      if(prevRadio) prevRadio.classList.remove('circle-clicked');
+      event.target.classList.add("circle-clicked"); 
+     });
+    }
