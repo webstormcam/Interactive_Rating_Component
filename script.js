9641b93efbaba7b7d95b9ce464a909b70787aec9
Cameron Chandler <webstormcam@gmail.com>
2022-03-27 13:47:58 -0400

    Figured out form
    Now I just gotta size it up on desktop :)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let selection;
 const pre_form = document.getElementById('pre-form')
+const post_form = document.getElementById('post-form')
+const special_words = document.getElementById('selected-wording')
 console.log(selection)
 const allChoices = document.querySelectorAll('.circle');
 for (let i = 0; i < allChoices.length; i++) {
@@ -11,13 +13,19 @@ for (let i = 0; i < allChoices.length; i++) {
      console.log(selection)
      });
     }
+    console.log(selection)
 
 
 
     function validateMyForm(){
       if(selection===undefined){
         alert('Please select a rating!')
-        return false
+       return false
     
-    } 
+    } else if(selection){
+      pre_form.style.display='none'
+      post_form.style.display='flex'
+      special_words.innerHTML=`You selected ${selection} out of 5`
+      console.log('ha ha')
+    }
   }
